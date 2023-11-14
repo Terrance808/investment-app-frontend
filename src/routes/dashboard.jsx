37beab9518d76f63c9ae9b19aea887ee179595a2
Terrance <terrance.rosas@protonmail.com>
2023-11-13 19:54:03 -0500

    update UI

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,43 +1,95 @@
 import React, { useState, useEffect } from 'react';
 import TradingViewAdvancedChart from '../components/dashboard/tradingview-advanced-chart';
-import '../tailwind.css';
 
 function Dashboard() {
+    const [stockTickerInput, setStockTickerInput] = useState('');
+    const [stockTicker, setStockTicker] = useState('');
+    const [stockPrice, setStockPrice] = useState(0);
 
+    
+    const handleStockInputChange = (event) => {
+        setStockTickerInput(event.target.value);
+    }
+    const searchStock = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:3000/search_stock', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ stock_ticker: stockTickerInput })
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setStockTicker(data.stock_ticker);
+            setStockPrice(data.stock_price);
+
+
+        } catch (error) {
+            console.log('Error during fetch:', error);
+        }
+        
+    }
+
+    
 
     return (
         <div>
-        <h1 class="text-3xl font-bold underline">Stock Dashboard</h1>
+        <h1 className="text-3xl font-bold underline">Stock Dashboard</h1>
         
             <div>
                 <h2>Portfolio</h2>
-                <button>Desposit</button>
+                
             </div>
 
-            <div class="flex">
-                <div class="w-3/5">
+            <div className="flex min-h-screen">
+                <div className="w-3/5">
                     <TradingViewAdvancedChart />
                 </div>
                 
-                <div class="w-1/5 border-2">
-                    <div class="flex justify-center h-5/10 bg-sky-500">
-                        <div class="h-8">
-                            <h2 class="text-xl">Funds</h2>
+                <div className="w-1/5 border-2">
+                    <div className="flex justify-center h-5/10 bg-sky-500">
+                        <div className="h-8">
+                            <h2 className="text-xl">Funds</h2>
                         </div>
                     
                     </div>
-                    <div class="flex justify-center">
-                        <h3 class="text-lg">Portfolio</h3>
+                    <div className="flex justify-center">
+                        <h3 className="text-lg">Portfolio</h3>
                     </div>
 
-                    <div>
+                    <div className="flex justify-center">
+                        <button>Desposit</button>
                     </div>
                 </div>
 
-                <div class="w-1/5 border-2">
-                    <input type="text" name="Search Bar" />
-                    <div class="flex justify-center">
-                        <h3 class="text-lg">Portfolio</h3>
+                <div className="w-1/5 border-2">
+                    <form onSubmit={searchStock}>
+                        <input 
+                            type="text" 
+                            placeholder="Search Bar" 
+                            name="searchBar" 
+                            className="w-full p-2" 
+                            value={stockTickerInput}
+                            onChange={handleStockInputChange}
+                        />
+                        <button type="submit" className="w-full p-2 bg-blue-500 text-white">Search</button>
+                    </form>
+                    <div className="flex justify-center">
+                        
+                        {stockTicker && stockPrice && (
+                            <div>
+                                <h3 className="text-lg">Current Stock Price</h3>
+                                <p>{stockTicker} : {stockPrice}</p>
+                            </div>
+                        )}
+                        <div>
+                            
+                        </div>
                     </div>
 
 
--- src/routes/homepage.jsx
+++ src/routes/homepage.jsx
@@ -6,7 +6,6 @@ import viteLogo from '../vite.svg';
 
 
 function Homepage() {
-    const [count, setCount] = useState(0);
 
     return (
         <>
@@ -18,18 +17,8 @@ function Homepage() {
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <h1>Welcome to our Investment App</h1>
+
             <Link to={"/login"}>
                 <button>Login</button>
             </Link>
